4b8ef5fa2c87d23989a71f35bc44f83ab91822d7
Rajeef <rajeefmk@gmail.com>
2014-05-20 12:14:55 +0530

    First Demo version

---
+++ android/MOOCTracker/src/us/jaaga/mooctracker/LandingPage.java
@@ -0,0 +1,19 @@
+package us.jaaga.mooctracker;
+
+
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LandingPage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	
+}
