375cd018e58407b7fa09c009cf97f7b639af2075
Ansal <ansal@bssatech.com>
2014-06-05 22:27:05 +0530

    new course view

--- web/mooctracker/static/mooc-tracker/views/student-main-view.js
+++ web/mooctracker/static/mooc-tracker/views/student-main-view.js
@@ -13,7 +13,8 @@ var app = app || {};
 
     events: {
 
-      'click #addCourseButton': 'showCourseForm'
+      'click #addCourseButton': 'showCourseForm',
+      'click #saveCourseButton': 'saveCourse'
 
     },
 
@@ -21,7 +22,10 @@ var app = app || {};
 
       // cache all DOM elements
       this.$courseForm = $('#courseForm');
-      this.$courseNameSelect= $('#courseName');
+      this.$courseNameSelect = $('#courseName');
+      this.$courseStart = $('#courseStart');
+      this.$courseEnd = $('#courseEnd');
+      this.$newCourseFormError = $('#newCourseFormError');
 
       // cache template
       this.courseSelectTemplate = _.template(
@@ -33,7 +37,16 @@ var app = app || {};
 
     },
 
+    // reset form elements
+    courseFormReset: function() {
+      this.$newCourseFormError.hide();
+      this.$courseNameSelect.val('0');
+      this.$courseStart.val('');
+      this.$courseEnd.val('');
+    },
+
     showCourseForm: function() {
+      this.courseFormReset();
       this.$courseForm.fadeToggle();
     },
 
@@ -44,6 +57,47 @@ var app = app || {};
           'courses': app.CourseCollection.models
         })
       );
+    },
+
+    buildCourseAttributes: function() {
+      // returning nothing means we are returning undefined
+      var course = this.$courseNameSelect.val();
+      // for selecting title of the select box
+      var elementId = this.$courseNameSelect.attr('id');
+      var courseTitle = $('#' + elementId  + ' option:selected')
+                        .text();
+      var courseStart = this.$courseStart.val();
+      var courseEnd = this.$courseEnd.val();
+      // validate all fields
+      // TODO: move to model validation
+      // it should be fatty models, thin views :)
+      if ( course === '0' ||
+            !courseTitle ||
+            !courseStart ||
+            !courseEnd
+      ) {
+        // return undefined
+        // TODO: can we make the returning a little better than this?
+        return undefined;
+      }
+      var studentCourseObject = {
+        course: course,
+        courseTitle: courseTitle,
+        courseStart: courseStart,
+        courseEnd: courseEnd
+      };
+      return studentCourseObject;
+    },
+
+    saveCourse: function() {
+      var course = this.buildCourseAttributes();
+      if(typeof course === 'undefined') {
+        // build attributes failed to return a course object
+        // this means that a form error has occured
+        // show the error message and return
+        this.$newCourseFormError.fadeIn();
+        return;
+      }
     }
 
   });
