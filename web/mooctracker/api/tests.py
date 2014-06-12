3f19d8fe01c61522c6d77c3a2b900e4426efafcf
Santu Mahapatra <santumahapatra@gmail.com>
2014-06-13 01:38:55 +0530

    Adding tests for the Course model and corresponding routes

--- web/mooctracker/api/tests.py
+++ web/mooctracker/api/tests.py
@@ -5,7 +5,10 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from students.models import Student
-from .serializers import StudentSerializer
+from courses.models import Course
+from .serializers import StudentSerializer, CourseSerializer
+
+# Tests for Student Model
 
 class ReadStudentTest(APITestCase):
   def setUp(self):
@@ -18,3 +21,47 @@ class ReadStudentTest(APITestCase):
   def test_can_read_student_detail(self):
     response = self.client.get(reverse('student-detail', args=[self.student.id]))
     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+# Tests for Course Model
+
+class CreateCourseTest(APITestCase):
+  def setUp(self):
+    self.course = Course.objects.create(course_title="Intro to Computer Science Build a Search Engine & a Social Network", url="https://www.udacity.com/course/cs101")
+    self.data = {'course_title': 'Intro to Computer Science Build a Search Engine & a Social Network', 'url': 'https://www.udacity.com/course/cs101'}
+
+  def test_can_create_course(self):
+    response = self.client.post(reverse('course-list'), self.data)
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class ReadCourseTest(APITestCase):
+  def setUp(self):
+    self.course = Course.objects.create(course_title="Intro to Computer Science Build a Search Engine & a Social Network", url="https://www.udacity.com/course/cs101")
+
+  def test_can_read_course_list(self):
+    response = self.client.get(reverse('course-list'))
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_can_read_course_detail(self):
+    response = self.client.get(reverse('course-detail', args=[self.course.id]))
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class UpdateCourseTest(APITestCase):
+  def setUp(self):
+    self.course = Course.objects.create(course_title="Intro to Computer Science Build a Search Engine & a Social Network", url="https://www.udacity.com/course/cs101")
+    self.updated_course = Course.objects.create(course_title="Intro to Computer Science", url="https://www.udacity.com/course/cs101")
+    self.data = CourseSerializer(self.updated_course).data
+  
+  def test_can_update_course(self):
+    response = self.client.put(reverse('course-detail', args=[self.course.id]), self.data)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class DeleteCourseTest(APITestCase):
+  def setUp(self):
+    self.course = Course.objects.create(course_title="Intro to Computer Science Build a Search Engine & a Social Network", url="https://www.udacity.com/course/cs101")    
+  
+  def test_can_update_course(self):
+    response = self.client.delete(reverse('course-detail', args=[self.course.id]))
+    self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
