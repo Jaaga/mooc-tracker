56077af7f13a7c88ca6893d4df948c18b808001d
Santu Mahapatra <santumahapatra@gmail.com>
2014-06-13 01:17:25 +0530

    Adding tests for the Student routes

--- web/mooctracker/api/tests.py
+++ web/mooctracker/api/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from students.models import Student
+from .serializers import StudentSerializer
+
+class ReadStudentTest(APITestCase):
+  def setUp(self):
+    self.student = Student.objects.create(name='ansal')
+
+  def test_can_read_student_list(self):
+    response = self.client.get(reverse('student-list'))
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_can_read_student_detail(self):
+    response = self.client.get(reverse('student-detail', args=[self.student.id]))
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
