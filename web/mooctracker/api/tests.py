3a99ddc38423b6f7068ed0355a0c3b9646a64a01
Santu Mahapatra <santumahapatra@gmail.com>
2014-06-13 01:50:09 +0530

    Adding tests for the Project model and routes

--- web/mooctracker/api/tests.py
+++ web/mooctracker/api/tests.py
@@ -6,7 +6,8 @@ from rest_framework.test import APITestCase
 
 from students.models import Student
 from courses.models import Course
-from .serializers import StudentSerializer, CourseSerializer
+from projects.models import Project
+from .serializers import StudentSerializer, CourseSerializer, ProjectSerializer
 
 # Tests for Student Model
 
@@ -64,4 +65,48 @@ class DeleteCourseTest(APITestCase):
   
   def test_can_update_course(self):
     response = self.client.delete(reverse('course-detail', args=[self.course.id]))
+    self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+# Tests for Project Model
+
+class CreateProjectTest(APITestCase):
+  def setUp(self):
+    self.project = Project.objects.create(project_name="Django Poll App ( Django version 1.6)", url="https://docs.djangoproject.com/en/1.6/intro/tutorial01/")
+    self.data = {'project_name': 'Django Poll App ( Django version 1.6)', 'url': 'https://docs.djangoproject.com/en/1.6/intro/tutorial01/'}
+
+  def test_can_create_project(self):
+    response = self.client.post(reverse('project-list'), self.data)
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class ReadProjectTest(APITestCase):
+  def setUp(self):
+    self.project = Project.objects.create(project_name="Django Poll App ( Django version 1.6)", url="https://docs.djangoproject.com/en/1.6/intro/tutorial01/")
+
+  def test_can_read_project_list(self):
+    response = self.client.get(reverse('project-list'))
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_can_read_project_detail(self):
+    response = self.client.get(reverse('project-detail', args=[self.project.id]))
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class UpdateProjectTest(APITestCase):
+  def setUp(self):
+    self.project = Project.objects.create(project_name="Django Poll App ( Django version 1.6)", url="https://docs.djangoproject.com/en/1.6/intro/tutorial01/")
+    self.updated_project = Project.objects.create(project_name="Django Poll App", url="https://docs.djangoproject.com/en/1.6/intro/tutorial01/")
+    self.data = ProjectSerializer(self.updated_project).data
+  
+  def test_can_update_project(self):
+    response = self.client.put(reverse('project-detail', args=[self.project.id]), self.data)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class DeleteProjectTest(APITestCase):
+  def setUp(self):
+    self.project = Project.objects.create(project_name="Django Poll App ( Django version 1.6)", url="https://docs.djangoproject.com/en/1.6/intro/tutorial01/")
+  
+  def test_can_update_project(self):
+    response = self.client.delete(reverse('project-detail', args=[self.project.id]))
     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
