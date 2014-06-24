dec17d284930c8296180e38fdba916dea5d4719b
Santu Mahapatra <santumahapatra@gmail.com>
2014-06-24 16:00:44 +0530

    Initializing mooctracker_rails app. Also augmenting .gitignore.

---
+++ web/mooctracker_rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MooctrackerRails::Application.config.secret_key_base = '27f4ffd4c323b871ff7b4f6ff3df6b0fabb501bd43da74bc57feba77061903f8169a99f884b069251dd60115e0aebe1d1931b7854ef076da3a83edd48c60a72e'
