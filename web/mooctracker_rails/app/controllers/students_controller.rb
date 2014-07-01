cffa5a2496e2de759a834bc01ca4c4884ff48f6d
prank7 <prashant.abhishek7g@gmail.com>
2014-07-01 10:39:53 +0530

    Add student model

---
+++ web/mooctracker_rails/app/controllers/students_controller.rb
@@ -0,0 +1,56 @@
+class StudentsController < ApplicationController
+  before_action :admin_user,     only: [:destroy, :create, :update]
+
+
+#On POST request to /students it creates a student with sent data
+  def create
+    @student = current_user.students.build(student_params)
+    if @student.save
+      render json: @student
+    else
+      @student_items = []
+    end
+  end
+
+
+#Edit action is left cause backbone handles edit on itself
+  def edit
+  end
+
+
+
+#Send a PUT/PATCH request on /students/id , to edit a particular student
+  def update
+    @student = Student.find(params[:id])
+    if @student.update_attributes(student_params)
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
+
+
+#Send a DELETE request on /students/id to destroy the student object
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to root_url
+  end
+
+
+	 
+
+
+	private
+
+	def student_params
+      params.require(:student).permit(:name, :email)
+    end
+
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
+end
--- web/mooctracker_rails/db/schema.rb
+++ web/mooctracker_rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630101902) do
+ActiveRecord::Schema.define(version: 20140701045859) do
 
   create_table "courses", force: true do |t|
     t.string   "title"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20140630101902) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "students", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
